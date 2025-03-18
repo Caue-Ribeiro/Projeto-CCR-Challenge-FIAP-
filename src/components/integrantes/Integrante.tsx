dc6e08dd72df9bf1c3c23ba01e8df2ef4fcee882
Ana Eliza <rm559544@fiap.com.br>
2025-03-17 21:28:57 -0300

    deleção de comentários pagina componente integrante

--- src/components/integrantes/Integrante.tsx
+++ src/components/integrantes/Integrante.tsx
@@ -1,9 +1,6 @@
 import Image from 'next/image'
 import { IntegranteProps } from '@/interfaces/IntegranteProps'
 
-//todo revisar e aplicar melhorias
-//todo aplicar acessibilidade
-
 const Integrante = ({
     nome,
     email,
@@ -18,13 +15,16 @@ const Integrante = ({
 }: IntegranteProps) => {
     return (
         <article className=" w-[90%]  mx-auto bg-brancoccr sm:mx-auto md:mx-auto lg:mx-auto xl:mx-auto mt-16  shadow-xl rounded-lg text-gray-900">
-            <div className="rounded-t-lg h-32 overflow-hidden bg-black"></div>
+            <div
+                className="rounded-t-lg h-32 overflow-hidden bg-black"
+                aria-hidden="true"
+            ></div>
 
             <figure className="mx-auto w-32 h-32 relative -mt-16 border-4 border-white rounded-full overflow-hidden">
                 <Image
                     className="object-cover object-center h-32"
                     src={imagem_perfil}
-                    alt="Woman looking front"
+                    alt={`Foto de perfil de ${nome}`}
                     width={128}
                     height={128}
                 />
@@ -51,20 +51,28 @@ const Integrante = ({
             </div>
 
             <div className="p-4 border-t mx-8 mt-2 flex justify-center gap-5">
-                <a href={github} target="_blank">
+                <a
+                    href={github}
+                    target="_blank"
+                    aria-label="Link para o perfil no GitHub"
+                >
                     <Image
                         src={imagem_github}
                         width={50}
                         height={50}
-                        alt="github logo"
+                        alt="Logo do GitHub"
                     />
                 </a>
-                <a href={linkedin} target="_blank">
+                <a
+                    href={linkedin}
+                    target="_blank"
+                    aria-label="Link para o perfil no LinkedIn"
+                >
                     <Image
                         src={imagem_linkedin}
                         width={50}
                         height={50}
-                        alt="linkedin logo"
+                        alt="Logo do LinkedIn"
                     />
                 </a>
             </div>
