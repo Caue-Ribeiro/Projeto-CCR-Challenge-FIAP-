9d9447fa29c3f4a7396cfb651aa59979f5113e7f
Ana Eliza <rm559544@fiap.com.br>
2025-05-06 21:41:45 -0300

    nova interface para propriedade objeto dados de estação

---
+++ src/interfaces/EstacaoTrensTypes.ts
@@ -0,0 +1,17 @@
+export interface PreverType {
+    Estação: string
+    Condições_Meteorológicas: string
+    Passageiros_Entrada: number
+    Passageiros_Saída: number
+    Problemas_Técnicos: number
+    Hora_decimal: number
+}
+
+export interface DadosEstacaoType {
+    CONDICAO_METEREOLOGICA: string
+    ESTACAO: string
+    HORA_DECIMAL: number
+    PASSAGEIROS_ENTRADA: number
+    PASSAGEIROS_SAIDA: number
+    PROBLEMA_TECNICO: number
+}
