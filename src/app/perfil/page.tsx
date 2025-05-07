83e63b10054f0f85bc0fbed4073f2b1bb3c85fc0
Cauê Ribeiro <rm559897@fiap.com.br>
2025-05-06 21:50:05 -0300

    carregamento de dados usuario/ funções de atualizar/deletar usuario

--- src/app/perfil/page.tsx
+++ src/app/perfil/page.tsx
@@ -1,11 +1,80 @@
+'use client'
+
 import Image from 'next/image'
-import avatar from '../../../public/images/avatar.png'
+import avatar from '../../../public/images/logo-CCR.png'
 import BotaoCustom from '@/components/BotaoCustom'
-import Form from 'next/form'
+import { useContextoGlobal } from '../context'
+import { lazy, Suspense, useState } from 'react'
+import { customFetchJava } from '@/utils'
+import { deletarSessao } from '../lib/sessao'
+import { redirect } from 'next/navigation'
+
+const ListaReports = lazy(() => import('@/components/ListaReports'))
 
 const Perfil = () => {
+    const {
+        dadosUsuario,
+        setDadosUsuario,
+        dadosNome,
+        setDadosNome,
+        dadosEmail,
+        setDadosEmail,
+    } = useContextoGlobal()
+
+    const [ativadorInput, setAtivadorInput] = useState(true)
+    const [carregandoAtualizar, setCarregandoAtualizar] = useState(false)
+    const [carregandoDelecao, setCarregandoDelecao] = useState(false)
+
+    const dadosAtualizados = {
+        nome: dadosNome,
+        email: dadosEmail,
+        senha: dadosUsuario?.senha,
+    }
+
+    const atualizarDados = async () => {
+        console.log(dadosUsuario?.id)
+
+        try {
+            setCarregandoAtualizar(true)
+            const resposta = await customFetchJava.put(
+                `/${dadosUsuario?.id}`,
+                dadosAtualizados
+            )
+
+            setAtivadorInput(true)
+
+            localStorage.setItem('login', JSON.stringify(resposta?.data))
+            setDadosUsuario(resposta?.data)
+        } catch (error) {
+            console.log(error)
+        }
+
+        setCarregandoAtualizar(false)
+    }
+
+    const deletarUsuario = async () => {
+        const confirmaDelecao = confirm(
+            'Tem certeza que deseja excluir sua conta?'
+        )
+
+        if (!confirmaDelecao) return
+
+        try {
+            setCarregandoDelecao(true)
+            await deletarSessao()
+            await customFetchJava.delete(`/${dadosUsuario?.id}`)
+            setDadosUsuario(null)
+            localStorage.removeItem('login')
+        } catch (error) {
+            console.log(error)
+        }
+
+        setCarregandoDelecao(false)
+        redirect('/')
+    }
+
     return (
-        <main className=" h-screen py-10  my-auto bg-brancoccr">
+        <main className=" h-screen py-10  my-auto bg-brancoccr overflow-y-auto">
             <section className=" lg:w-[80%] md:w-[90%] w-[96%] mx-auto flex gap-4 ">
                 <article className="lg:w-[88%] sm:w-[88%] w-full mx-auto shadow-2xl p-4 rounded-xl h-fit self-center">
                     <div>
@@ -13,55 +82,22 @@ const Perfil = () => {
                             <h1 className="lg:text-3xl md:text-2xl text-xl font-extrabold mb-2 text-pretoccr text-center">
                                 Perfil
                             </h1>
-                        </header>
-
-                        <Form action={''}>
                             <div className="w-full rounded-sm items-center">
                                 <div className="mx-auto flex justify-center">
                                     <Image
                                         src={avatar}
                                         alt="foto perfil usuário"
-                                        className=" w-36 h-36 bg-blue-300/20 rounded-full"
+                                        className=" w-36 h-36 bg-blue-300/20 rounded-full "
                                     />
-                                    <div className="absolute bg-white/90 rounded-full w-6 h-6 text-center ml-28 mt-4">
-                                        <input
-                                            type="file"
-                                            name="profile"
-                                            id="upload_profile"
-                                            hidden
-                                            required
-                                            aria-label="Atualizar foto de perfil"
-                                        />
-                                        <label htmlFor="upload_profile">
-                                            <svg
-                                                data-slot="icon"
-                                                className="w-6 h-5 text-vermelhoccr"
-                                                fill="none"
-                                                strokeWidth="1.5"
-                                                stroke="currentColor"
-                                                viewBox="0 0 24 24"
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                aria-hidden="true"
-                                            >
-                                                <path
-                                                    strokeLinecap="round"
-                                                    strokeLinejoin="round"
-                                                    d="M6.827 6.175A2.31 2.31 0 0 1 5.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 0 0-1.134-.175 2.31 2.31 0 0 1-1.64-1.055l-.822-1.316a2.192 2.192 0 0 0-1.736-1.039 48.774 48.774 0 0 0-5.232 0 2.192 2.192 0 0 0-1.736 1.039l-.821 1.316Z"
-                                                ></path>
-                                                <path
-                                                    strokeLinecap="round"
-                                                    strokeLinejoin="round"
-                                                    d="M16.5 12.75a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0ZM18.75 10.5h.008v.008h-.008V10.5Z"
-                                                ></path>
-                                            </svg>
-                                        </label>
-                                    </div>
                                 </div>
+                                <h2 className="text-center text-3xl mt-1 font-semibold text-pretoccr">
+                                    {dadosUsuario?.nome}
+                                </h2>
                             </div>
-                            <h2 className="text-center mt-1 font-semibold dark:text-gray-300">
-                                Atualize foto perfil
-                            </h2>
-                            <div className="flex flex-col lg:flex-row gap-2 justify-center w-full">
+                        </header>
+
+                        <div>
+                            <form className="flex flex-col lg:flex-row gap-2 justify-center w-full">
                                 <div className="w-full mb-4 mt-6">
                                     <label
                                         htmlFor="usuario"
@@ -75,6 +111,11 @@ const Perfil = () => {
                                         className="mt-2 p-4 w-full border-1 rounded-lg text-pretoccr"
                                         placeholder="Usuário"
                                         aria-label="Campo de entrada para o nome de usuário"
+                                        disabled={ativadorInput}
+                                        value={dadosNome}
+                                        onChange={e =>
+                                            setDadosNome(e.target.value)
+                                        }
                                     />
                                 </div>
                                 <div className="w-full mb-4 lg:mt-6">
@@ -90,19 +131,69 @@ const Perfil = () => {
                                         className="mt-2 p-4 w-full border-1 rounded-lg text-pretoccr"
                                         placeholder="E-mail"
                                         aria-label="Campo de entrada para o e-mail"
+                                        disabled={ativadorInput}
+                                        value={dadosEmail}
+                                        onChange={e =>
+                                            setDadosEmail(e.target.value)
+                                        }
                                     />
                                 </div>
-                            </div>
+                            </form>
 
                             <div className="w-full  text-white  rounded-md border transition  border-vermelhoccr bg-vermelhoccr text-lg font-semibold hover:bg-transparent hover:text-pretoccr focus:ring-3 focus:outline-hidden">
                                 <BotaoCustom
-                                    nome="Atualizar"
+                                    onClick={() => setAtivadorInput(false)}
+                                    nome="Mudar dados"
+                                    aria_label="Botão para mudar informações"
+                                    type="button"
+                                    className="w-full p-4"
+                                />
+                            </div>
+                            <div className="w-full mt-2 text-white  rounded-md border transition  border-vermelhoccr bg-vermelhoccr text-lg font-semibold hover:bg-transparent hover:text-pretoccr focus:ring-3 focus:outline-hidden">
+                                <BotaoCustom
+                                    nome={
+                                        carregandoAtualizar
+                                            ? 'Atualizando...'
+                                            : 'Atualizar'
+                                    }
                                     type="submit"
                                     className="w-full p-4"
                                     aria-label="Botão para atualizar o perfil"
+                                    onClick={atualizarDados}
                                 />
                             </div>
-                        </Form>
+
+                            <div className="max-w-4xl h-80 my-8 mx-auto p-6">
+                                <h2 className="text-2xl text-center font-bold mb-6">
+                                    Meus Reports
+                                </h2>
+                                <div className="h-60 overflow-y-auto">
+                                    <Suspense
+                                        fallback={
+                                            <h1 className="text-2xl text-center">
+                                                Carregando lista...
+                                            </h1>
+                                        }
+                                    >
+                                        <ListaReports />
+                                    </Suspense>
+                                </div>
+                            </div>
+
+                            <div className="w-full mt-2 text-white  rounded-md border transition  border-vermelhoccr bg-vermelhoccr text-lg font-semibold hover:bg-transparent hover:text-pretoccr focus:ring-3 focus:outline-hidden">
+                                <BotaoCustom
+                                    onClick={deletarUsuario}
+                                    nome={
+                                        carregandoDelecao
+                                            ? 'DELETANDO...'
+                                            : 'DELETAR CONTA'
+                                    }
+                                    type="button"
+                                    aria_label="Botão para deletar usuário"
+                                    className="w-full p-4"
+                                />
+                            </div>
+                        </div>
                     </div>
                 </article>
             </section>
