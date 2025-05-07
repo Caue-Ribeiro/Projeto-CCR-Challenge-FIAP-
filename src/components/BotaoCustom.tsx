9ebd5359befd697513fd7bb36d437fefb52f9b07
Ana Eliza <rm559544@fiap.com.br>
2025-05-06 21:40:16 -0300

    adição de parametro ao componente botão customizado

--- src/components/BotaoCustom.tsx
+++ src/components/BotaoCustom.tsx
@@ -1,8 +1,21 @@
 import { BotaoProps } from '@/interfaces/BotaoCustomProps'
 
-const BotaoCustom = ({ nome, className, type, aria_label }: BotaoProps) => {
+const BotaoCustom = ({
+    nome,
+    className,
+    type,
+    aria_label,
+    onClick,
+    desabilitado,
+}: BotaoProps) => {
     return (
-        <button type={type} className={className} aria-label={aria_label}>
+        <button
+            type={type}
+            className={className}
+            aria-label={aria_label}
+            onClick={onClick}
+            disabled={desabilitado}
+        >
             {nome}
         </button>
     )
