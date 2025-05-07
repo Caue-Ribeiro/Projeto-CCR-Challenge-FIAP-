af0eac1d262934e01465c32fa54f578122df6c21
Ana Eliza <rm559544@fiap.com.br>
2025-05-06 21:33:42 -0300

    pg. estação&trens: criação componente de busca + importação

--- src/app/estacao&trens/page.tsx
+++ src/app/estacao&trens/page.tsx
@@ -1,29 +1,18 @@
 import Image from 'next/image'
 import estacaoTrensIMG from '../../../public/images/estacao_trens.webp'
-import BotaoCustom from '@/components/BotaoCustom'
+import BuscaInputEstacaoTrens from '@/components/BuscaInputEstacaoTrens'
+import DashBoard from '@/components/DashBoard'
 
 const EstacaoTrens = () => {
     return (
-        <main className="flex flex-col-reverse md:flex-row h-screen">
+        <main className="flex flex-col-reverse md:flex-row h-screen relative overflow-x-hidden overflow-y-auto">
             <section className="w-full md:w-1/2 flex flex-col justify-center items-center bg-brancoccr text-pretoccr p-8">
                 <header>
                     <h1 className="text-3xl font-bold mb-6 text-center md:text-left">
                         Pesquise a estação
                     </h1>
                 </header>
-                <input
-                    type="search"
-                    placeholder="Digite sua busca..."
-                    className="w-full max-w-md p-3 rounded-lg bg-brancoccr text-pretoccr border border-cinzaccr focus:outline-none focus:ring-2 focus:ring-cinzaccr placeholder:text-pretoccr"
-                    aria-labelledby="campo de busca"
-                />
-
-                <BotaoCustom
-                    aria_label="Exibir informação sobre a estação"
-                    className="inline-block mt-3.5 shrink-0 rounded-md border border-vermelhoccr bg-vermelhoccr px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-pretoccr focus:ring-3 focus:outline-hidden"
-                    type="submit"
-                    nome="Exibir informação"
-                />
+                <BuscaInputEstacaoTrens />
             </section>
 
             <section className="w-full h-full md:w-1/2 relative  md:h-auto">
@@ -36,6 +25,8 @@ const EstacaoTrens = () => {
                 />
                 <div className="absolute inset-0 bg-gradient-to-t from-brancoccr to-transparent md:bg-gradient-to-r"></div>
             </section>
+
+            <DashBoard />
         </main>
     )
 }
---
+++ src/components/BuscaInputEstacaoTrens.tsx
@@ -0,0 +1,85 @@
+'use client'
+
+import { FormEvent, useRef, useState } from 'react'
+import BotaoCustom from './BotaoCustom'
+import { customFetchPython } from '@/utils'
+import { DadosEstacaoType, PreverType } from '@/interfaces/EstacaoTrensTypes'
+import { useContextoGlobal } from '@/app/context'
+
+const BuscaInputEstacaoTrens = () => {
+    const { setDadosEstacao, setDadosPredicao } = useContextoGlobal()
+    const [carregando, setCarregando] = useState<boolean>(false)
+
+    const valorRef = useRef<HTMLInputElement>(null)
+
+    const controleFormulario = async (e: FormEvent): Promise<void> => {
+        e.preventDefault()
+        setCarregando(true)
+        const VALOR_BUSCA = valorRef.current?.value
+
+        try {
+            const { data } = await customFetchPython.get('/dados_estacao?', {
+                params: {
+                    estacao: VALOR_BUSCA,
+                },
+            })
+
+            const {
+                CONDICAO_METEREOLOGICA,
+                ESTACAO,
+                HORA_DECIMAL,
+                PASSAGEIROS_ENTRADA,
+                PASSAGEIROS_SAIDA,
+                PROBLEMA_TECNICO,
+            }: DadosEstacaoType = data[0] || {}
+
+            const obj: PreverType = {
+                Estação: ESTACAO,
+                Condições_Meteorológicas: CONDICAO_METEREOLOGICA,
+                Passageiros_Entrada: PASSAGEIROS_ENTRADA,
+                Passageiros_Saída: PASSAGEIROS_SAIDA,
+                Problemas_Técnicos: PROBLEMA_TECNICO,
+                Hora_decimal: HORA_DECIMAL,
+            }
+
+            const { data: prever } = await customFetchPython.post(
+                '/prever',
+                obj,
+                {
+                    headers: { 'Content-Type': 'application/json' },
+                }
+            )
+
+            setDadosEstacao(data)
+            setDadosPredicao(prever)
+            setCarregando(false)
+        } catch (error) {
+            setCarregando(false)
+            const err = error as Error
+            alert(
+                `Houve um erro, valor não encontrado. Favor refazer a pesquisa!`
+            )
+            console.log(err.message)
+        }
+    }
+
+    return (
+        <form className="text-center" onSubmit={controleFormulario}>
+            <input
+                ref={valorRef}
+                type="text"
+                placeholder="Digite sua busca..."
+                className="w-full max-w-md p-3 rounded-lg bg-brancoccr text-pretoccr border border-cinzaccr focus:outline-none focus:ring-2 focus:ring-cinzaccr placeholder:text-pretoccr"
+                aria-label="campo de busca"
+            />
+
+            <BotaoCustom
+                aria_label="Exibir informação sobre a estação"
+                className="inline-block mt-3.5 shrink-0 rounded-md border border-vermelhoccr bg-vermelhoccr px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-pretoccr focus:ring-3 focus:outline-hidden"
+                type="submit"
+                nome={`${carregando ? 'Buscando...' : 'Exibir informação'}`}
+            />
+        </form>
+    )
+}
+export default BuscaInputEstacaoTrens
