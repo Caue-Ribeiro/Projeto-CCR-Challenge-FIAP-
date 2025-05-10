f0bd1a968e8016397dd67596b1a4729fe7f7bd2a
Cauê Ribeiro <rm559897@fiap.com.br>
2025-05-10 13:11:14 -0300

    dados estações / interface dados estações

---
+++ src/data/estacoes.ts
@@ -0,0 +1,21 @@
+import { EstacoesProps } from '@/interfaces/Estacoes'
+
+export const estacoes: Readonly<EstacoesProps[]> = [
+    { valor: 'santo amaro', nome: 'Santo Amaro' },
+    { valor: 'aacd - servidor', nome: 'Aacd - Servidor' },
+    { valor: 'adolfo pinheiro', nome: 'Adolfo Pinheiro' },
+    { valor: 'borba gato', nome: 'Borba Gato' },
+    { valor: 'hospital são paulo', nome: 'Hospital São Paulo' },
+    { valor: 'brooklin', nome: 'Brooklin' },
+    { valor: 'moema', nome: 'Moema' },
+    { valor: 'chacara klabin', nome: 'Chacara Klabin' },
+    { valor: 'giovanni gronchi', nome: 'Giovanni Gronchi' },
+    { valor: 'campo belo', nome: 'Campo Belo' },
+    { valor: 'largo treze', nome: 'Largo Treze' },
+    { valor: 'eucaliptos', nome: 'Eucaliptos' },
+    { valor: 'capão redondo', nome: 'Capão Redondo' },
+    { valor: 'vila das belezas', nome: 'Vila Das Belezas' },
+    { valor: 'alto da boa vista', nome: 'Alto Da Boa Vista' },
+    { valor: 'campo limpo', nome: 'Campo Limpo' },
+    { valor: 'santa cruz', nome: 'Santa Cruz' },
+]
---
+++ src/interfaces/Estacoes.ts
@@ -0,0 +1,4 @@
+export interface EstacoesProps {
+    valor: string
+    nome: string
+}
