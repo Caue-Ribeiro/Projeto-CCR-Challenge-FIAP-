10470205df39f498442fede3bd0b46e2d1f63c59
Cauê Ribeiro <rm559897@fiap.com.br>
2025-05-11 12:02:36 -0300

    url railway

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,8 +2,7 @@ import axios, { AxiosInstance } from 'axios'
 
 const URL_PYTHON = 'https://api-python-ccr.onrender.com/'
 
-const URL_JAVA =
-    'https://15c8-2804-14c-bf48-8ad5-c045-78fc-cc3f-a4c1.ngrok-free.app/usuario'
+const URL_JAVA = 'api-quarkus-ccr-production.up.railway.app/usuario'
 
 export const customFetchPython: AxiosInstance = axios.create({
     baseURL: URL_PYTHON,
