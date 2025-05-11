30447ef7e3b78c2a7ab6780d05b1675bfc8d9f45
Cauê Ribeiro <rm559897@fiap.com.br>
2025-05-10 23:24:04 -0300

    url ngrok teste

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,7 +2,8 @@ import axios, { AxiosInstance } from 'axios'
 
 const URL_PYTHON = 'https://api-python-ccr.onrender.com/'
 
-const URL_JAVA = 'http://localhost:8080/usuario'
+const URL_JAVA =
+    'https://15c8-2804-14c-bf48-8ad5-c045-78fc-cc3f-a4c1.ngrok-free.app/usuario'
 
 export const customFetchPython: AxiosInstance = axios.create({
     baseURL: URL_PYTHON,
